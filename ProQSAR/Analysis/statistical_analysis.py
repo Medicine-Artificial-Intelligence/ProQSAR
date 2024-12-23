8102215809bfc6dc888a1b035771d8c09faad2fd
Medicine-Artificial-Intelligence <medaiump@gmail.com>
2024-12-23 17:38:37 +0700

    transfer from lab3

--- ProQSAR/Analysis/statistical_analysis.py
+++ ProQSAR/Analysis/statistical_analysis.py
@@ -6,7 +6,6 @@ import seaborn as sns
 from copy import deepcopy
 from scipy import stats
 from scipy.stats import spearmanr, levene
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, precision_score, recall_score
 from statsmodels.stats.anova import AnovaRM
 from statsmodels.stats.libqsturng import psturng, qsturng
 from matplotlib import cm
@@ -29,21 +28,21 @@ class StatisticalAnalysis:
         """
         if isinstance(scoring, str):
             scoring = [scoring]
-    
+
         scoring_dfs = {}
 
         for score in scoring:
             score_df = report_df[report_df.index.str.startswith(f"{score}_fold")]
             scoring_dfs[f"{score}"] = score_df
-    
+
         return scoring_dfs
-    
+
     @staticmethod
     def extract_scoring_dfs(report_df: pd.DataFrame, scoring: list | str):
 
         if isinstance(scoring, str):
             scoring = [scoring]
-    
+
         filtered_dfs = []
 
         for score in scoring:
@@ -54,16 +53,17 @@ class StatisticalAnalysis:
         scoring_dfs = pd.concat(filtered_dfs)
 
         # Melt the dataframe to long format
-        df_long = scoring_dfs.melt(
-            id_vars=["Scoring", "index"],
-            var_name="Method",
-            value_name="Value"
+        df_long = scoring_dfs.reset_index().melt(
+            id_vars=["Scoring", "index"], var_name="Method", value_name="Value"
         )
+        df_long.rename(columns={"index": "Fold"}, inplace=True)
 
         return df_long
-    
+
     @staticmethod
-    def check_variance_homogeneity(report_df: pd.DataFrame, scoring: list | str, levene_test: bool = True):
+    def check_variance_homogeneity(
+        report_df: pd.DataFrame, scoring: list | str, levene_test: bool = True
+    ):
 
         if isinstance(scoring, str):
             scoring = [scoring]
@@ -78,18 +78,22 @@ class StatisticalAnalysis:
             variances_by_method = metric_data.groupby("Method")["Value"].var()
             max_fold_diff = variances_by_method.max() / variances_by_method.min()
 
-            result.append({
-                'metric': metric,
-                'variance_fold_difference': max_fold_diff,
-            })
+            metric_result = {
+                "metric": metric,
+                "variance_fold_difference": max_fold_diff,
+            }
             # Perform Levene's test if specified
             p_value = None
             if levene_test:
-                groups = [group["Value"].values for _, group in metric_data.groupby('method')]
+                groups = [
+                    group["Value"].values for _, group in metric_data.groupby("Method")
+                ]
                 stat, p_value = levene(*groups)
-                result.append({'p_value': p_value})
-                
-        result_df = pd.DataFrame(result).set_index('metric')
+                metric_result["p_value"] = p_value
+
+            result.append(metric_result)
+
+        result_df = pd.DataFrame(result).set_index("metric")
 
         return result_df
 
@@ -115,26 +119,41 @@ class StatisticalAnalysis:
 
         # Normalize values by subtracting the mean for each method
         df_norm = deepcopy(report_df)
-        df_norm["Value"] = df_norm.groupby(["Method", "Scoring"])["Value"].transform(lambda x: x - x.mean())
+        df_norm["Value"] = df_norm.groupby(["Method", "Scoring"])["Value"].transform(
+            lambda x: x - x.mean()
+        )
 
-        sns.set_context('notebook', font_scale=1.5)
-        sns.set_style('whitegrid')
-    
-        scorings = df_norm['Scoring'].unique()
+        sns.set_context("notebook", font_scale=1.5)
+        sns.set_style("whitegrid")
+
+        scorings = df_norm["Scoring"].unique()
         n_scorings = len(scorings)
-    
+
         fig, axes = plt.subplots(2, n_scorings, figsize=(20, 10))
-    
+
         for i, metric in enumerate(scorings):
             ax = axes[0, i]
-            sns.histplot(df_norm[df_norm["Scoring"] == metric]['Value'], kde=True, ax=ax)
-            ax.set_title(f'{metric}', fontsize=16)
-    
+            sns.histplot(
+                df_norm[df_norm["Scoring"] == metric]["Value"], kde=True, ax=ax
+            )
+            ax.set_title(f"{metric}", fontsize=16)
+
         for i, metric in enumerate(scorings):
             ax = axes[1, i]
-            metric_data = df_norm[df_norm["Scoring"] == metric]['Value']
+            metric_data = df_norm[df_norm["Scoring"] == metric]["Value"]
             stats.probplot(metric_data, dist="norm", plot=ax)
             ax.set_title("")
-    
+
         plt.tight_layout()
 
+    def make_ANOVARM(report_df, scoring: list | str):
+        
+        if isinstance(scoring, str):
+            scoring = [scoring]
+
+        for i, stat in enumerate(scoring):
+            model = AnovaRM(data=report_df, depvar=stat, subject='cv_cycle', within=['method']).fit()
+            p_value = model.anova_table['Pr > F'].iloc[0]
+            
+        #### co nen code visualize? neu co thi xai lai code cua model_validation hay code rieng moi?
+        
